beb3c725468fc0c1dea376f71ae5890b556f06ed
Brian Tochterman <btoch23@gmail.com>
2023-09-22 13:54:10 -0500

    Added extra button functionality

--- circuit.js
+++ circuit.js
@@ -1,6 +1,87 @@
 let addToCart = document.querySelectorAll('.addToCart');
+
+let shortCard = document.getElementById('shortCard');
+let medCard = document.getElementById('medCard');
+let longCard = document.getElementById('longCard');
+
+let short = document.getElementById('short');
+let med = document.getElementById('med');
+let long = document.getElementById('long');
+let timeButtons = document.querySelectorAll('.timeSet');
+
 let exercises = document.getElementById('exerciseList');
+
+let start = document.getElementById('startWorkout');
+
 const chosenExercises = [];
+
+for (let i of timeButtons) {
+    i.addEventListener('click', function() {
+        if (this.id === 'short') {
+            if (!med.disabled && !long.disabled) {
+                med.disabled = true;
+                long.disabled = true;
+            } else {
+                med.disabled = false;
+                long.disabled = false;
+            }
+            if (this.innerText === 'Choose Me') {
+                this.innerText = 'Undo';
+            } else {
+                this.innerText = 'Choose Me';
+            }
+            if (!medCard.style.opacity) {
+                medCard.style.opacity = '25%';
+                longCard.style.opacity = '25%';
+            } else {
+                medCard.style.opacity = null;
+                longCard.style.opacity = null;
+            }
+        } 
+        if (this.id === 'med') {
+            if (!short.disabled && !long.disabled) {
+                short.disabled = true;
+                long.disabled = true;
+            } else {
+                short.disabled = false;
+                long.disabled = false;
+            }
+            if (this.innerText === 'Choose Me') {
+                this.innerText = 'Undo';
+            } else {
+                this.innerText = 'Choose Me';
+            }
+            if (!shortCard.style.opacity) {
+                shortCard.style.opacity = '25%';
+                longCard.style.opacity = '25%';
+            } else {
+                shortCard.style.opacity = null;
+                longCard.style.opacity = null;
+            }
+        } 
+        if (this.id === 'long') {
+            if (!med.disabled && !short.disabled) {
+                med.disabled = true;
+                short.disabled = true;
+            } else {
+                med.disabled = false;
+                short.disabled = false;
+            }
+            if (this.innerText === 'Choose Me') {
+                this.innerText = 'Undo';
+            } else {
+                this.innerText = 'Choose Me';
+            }
+            if (!medCard.style.opacity) {
+                medCard.style.opacity = '25%';
+                shortCard.style.opacity = '25%';
+            } else {
+                medCard.style.opacity = null;
+                shortCard.style.opacity = null;
+            }
+        } 
+    })
+}
 
 for (let i of addToCart) {
     i.addEventListener('click', function() {
@@ -22,6 +103,12 @@ for (let i of addToCart) {
 }
 
 function makeList() {
+    if (chosenExercises.length === 0) {
+        start.disabled = true;
+    } else {
+        start.disabled = false;
+        start.innerText = 'Start Workout';
+    }
     for (let i of chosenExercises) {
         let exercise = document.createElement('li');
         exercise.textContent = i;
@@ -46,18 +133,31 @@ function removeExercise(event) {
     for (let i of addToCart) {
         if (i.disabled && i.previousElementSibling.innerText === ex) {
             i.disabled = false;
+            i.innerText = 'Add to Circuit';
         }
     }
+    if (chosenExercises.length === 0) {
+        let empty = document.createElement('li');
+        empty.textContent = 'Your circuit is empty';
+        exercises.appendChild(empty);
+    }
 }
+    
 
 function startWorkout() {
     alert('Your workout starts...NOW! Hope you remembered to stretch!');
     removeList();
+    chosenExercises.length = 0;
     for (let i of addToCart) {
         if (i.disabled) {
             i.disabled = false;
         }
-       }
+    }
+    let empty = document.createElement('li');
+    empty.textContent = 'Your circuit is empty';
+    exercises.appendChild(empty);
+    start.disabled = true;
+    start.innerText = 'Add some exercises!';
 }
 
 
