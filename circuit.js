7aed51c39470e3fddc2ec241b0f35143b9a82283
Brian Tochterman <btoch23@gmail.com>
2023-10-02 17:37:26 -0500

    Added the API to populate exercises based on muscle

--- circuit.js
+++ circuit.js
@@ -23,12 +23,43 @@ const timerElement = document.getElementById("timer");
 // Change this to the desired countdown time
 let countdownTime;
 let circuitLength;
+//The buttons that choose the muscle type
+const muscleBtns = document.querySelectorAll('.muscle');
+for (let i of muscleBtns) {
+    i.disabled = true;
+}
+//The div containing the exercises fetched from the API
+const exerciseDiv = document.querySelector('#chosenExerciseList');
+
+//FETCH DATA FROM THE API
+let muscle = '';
+const apiKey = process.env.API_KEY;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': apiKey,
+		'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+async function getExercises() {
+    const url = `https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=${muscle}`;
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const jsonResult = JSON.parse(result);
+        console.log(jsonResult);
+        displayExercises(jsonResult);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 //ADDS EVENT LISTENERS TO EACH TIME BUTTON TO DISABLE AND FADE THE NON-CHOSEN OPTIONS
-//ALSO ACTIVATES THE ADDTOCART BUTTONS
+//ALSO ACTIVATES THE MUSCLE BUTTONS
 for (let i of timeButtons) {
     i.addEventListener('click', function() {
-        for (let i of addToCart) {
+        for (let i of muscleBtns) {
             i.disabled = false;
         }
         if (this.id === 'short') {
@@ -65,32 +96,100 @@ for (let i of timeButtons) {
             this.disabled = true;
         } 
     })
-}
+};
 
-//ADDS AN EVENT LISTENER TO EACH 'ADD TO CIRCUIT' BUTTON AND ADDS CHOSEN EXERCISE TO THE EXERCISE ARRAY
-for (let i of addToCart) {
-    i.disabled = true;
-    i.addEventListener('click', function() {
-        i.disabled = true;
-        i.innerText = 'Added to Circuit!'
-        let workout = this.previousElementSibling.innerHTML;
-        if (chosenExercises.length === circuitLength - 1) {
-            for (let i of addToCart) {
-                i.disabled = true;
+//EVENT LISTENERS FOR MUSCLE BUTTONS
+const bicepBtn = document.querySelector('#bis');
+const tricepBtn = document.querySelector('#tris');
+const glutesBtn = document.querySelector('#glutes');
+const hamstringsBtn = document.querySelector('#hamstrings');
+const absBtn = document.querySelector('#abs');
+
+bicepBtn.addEventListener('click', () => {
+    muscle = 'biceps';
+    removeExercises();
+    getExercises();
+});
+tricepBtn.addEventListener('click', () => {
+    muscle = 'triceps';
+    removeExercises();
+    getExercises();
+});
+glutesBtn.addEventListener('click', () => {
+    muscle = 'glutes';
+    removeExercises();
+    getExercises();
+});
+hamstringsBtn.addEventListener('click', () => {
+    muscle = 'hamstrings';
+    removeExercises();
+    getExercises();
+});
+abs.addEventListener('click', () => {
+    muscle = 'abdominals';
+    removeExercises();
+    getExercises();
+});
+
+//Populates the exercises from the API in the exercise div
+function displayExercises(exercises) {
+    for (let i = 0; i < 6; i++) {
+        const exerciseCard = document.createElement('div');
+        exerciseCard.className = 'card mb-4 h-100';
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        const col = document.createElement('div');
+        col.className = 'col-4';
+        const title = document.createElement('h5');
+        title.className = 'card-title';
+        title.textContent = exercises[i].name;
+        const difficulty = document.createElement('header');
+        difficulty.className = 'card-header';
+        difficulty.textContent = exercises[i].difficulty;
+        const instructions = document.createElement('p');
+        instructions.className = 'card-text';
+        instructions.textContent = exercises[i].instructions;
+        const button = document.createElement('button');
+        button.className = 'btn addToCart';
+        button.type = 'button';
+        button.innerText = 'Add To Circuit!';
+        cardBody.appendChild(title);
+        cardBody.appendChild(instructions);
+        cardBody.appendChild(button);
+        exerciseCard.appendChild(difficulty);
+        exerciseCard.appendChild(cardBody);
+        col.appendChild(exerciseCard);
+        exerciseDiv.appendChild(col);
+        button.addEventListener('click', () => {
+            button.disabled = true;
+            button.innerText = 'Added to Circuit!';
+            let workout = exercises[i].name;
+            if (chosenExercises.length === circuitLength - 1) {
+                let cartBtns = document.querySelectorAll('.addToCart');
+                for (let i of cartBtns) {
+                    i.disabled = true;
+                }
             }
-        }
-        try {
-            if (chosenExercises.includes(workout)) {
-                throw new Error('Exercise already chosen!');
-            } else {
-                chosenExercises.push(workout);
-                removeList();
-                makeList();
+            try {
+                if (chosenExercises.includes(workout)) {
+                    throw new Error('Exercise already chosen!');
+                } else {
+                    chosenExercises.push(workout);
+                    console.log(chosenExercises);
+                    removeList();
+                    makeList();
+                }
+            } catch(error) {
+                alert(error.message);
             }
-        } catch(error) {
-            alert(error.message);
-        }
-    });
+        });
+    }
+}
+//removes the exercise list everytime a muscle button is clicked to prevent too many cards from populating
+function removeExercises() {
+    while (exerciseDiv.firstChild) {
+        exerciseDiv.removeChild(exerciseDiv.firstChild);
+    }
 }
 
 //HANDLES THE DISABLED STATE OF THE START BUTTON AND POPULATES THE CHOSEN EXERCISES INTO THE SIDEBAR
@@ -105,7 +204,7 @@ function makeList() {
         let exercise = document.createElement('li');
         exercise.textContent = i;
         exercise.className = 'list-group-item';
-        exercise.addEventListener('click', removeExercise);
+        // exercise.addEventListener('click', removeExercise);
         exercises.appendChild(exercise);
     }
 }
@@ -117,28 +216,29 @@ function removeList() {
     }
 }
 
-//REMOVES A CHOSEN EXERCISE FROM THE LIST AND THE ARRAY
-function removeExercise(event) {
-    const ex = event.currentTarget.textContent;
-    let e = chosenExercises.indexOf(ex);
-    chosenExercises.splice(e, 1);
-    removeList();
-    makeList();
-    for (let i of addToCart) {
-        let exercise = i.previousElementSibling.innerHTML
-        if (i.disabled && i.previousElementSibling.innerText === ex) {
-            i.disabled = false;
-            i.innerText = 'Add to Circuit';
-        } if (i.disabled && chosenExercises.length < circuitLength && !chosenExercises.includes(exercise)) {
-            i.disabled = false;
-        }
-    }
-    if (chosenExercises.length === 0) {
-        let empty = document.createElement('li');
-        empty.textContent = 'Your circuit is empty';
-        exercises.appendChild(empty);
-    }
-}
+// //REMOVES A CHOSEN EXERCISE FROM THE LIST AND THE ARRAY
+// function removeExercise(event) {
+//     const ex = event.currentTarget.textContent;
+//     let e = chosenExercises.indexOf(ex);
+//     chosenExercises.splice(e, 1);
+//     removeList();
+//     makeList();
+//     for (let i of addToCart) {
+//         let sibling = i.previousElementSibling;
+//         let exercise = sibling.previousElementSibling.innerText;
+//         if (i.disabled && sibling.previousElementSibling.innerText === ex) {
+//             i.disabled = false;
+//             i.innerText = 'Add to Circuit';
+//         } if (i.disabled && chosenExercises.length < circuitLength && !chosenExercises.includes(exercise)) {
+//             i.disabled = false;
+//         }
+//     }
+//     if (chosenExercises.length === 0) {
+//         let empty = document.createElement('li');
+//         empty.textContent = 'Your circuit is empty';
+//         exercises.appendChild(empty);
+//     }
+// }
     
 //NOTIFIES USER THAT WORKOUT IS BEGINNING, STARTS THE TIMER, AND RESETS EVERYTHING
 function startWorkout() {
